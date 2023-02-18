451f708a4e6fa18f041684aaabbbe47cf96a080b
Albert Kison <albert.kison@student.griffith.ie>
2023-02-18 22:09:19 +0000

    Add gravity

--- canvas.js
+++ canvas.js
@@ -4,6 +4,10 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext("2d");
 
+const gravity = 1;
+const frictionY = 0.9;
+const frictionX = 0.5;
+
 function Circle(x, y, dx, dy, radius) {
     this.x = x;
     this.y = y;
@@ -26,18 +30,34 @@ function Circle(x, y, dx, dy, radius) {
     }
 
     this.update = function() {
-        
+        if (this.y + this.radius + this.dy > canvas.height) {
+            this.dy = -this.dy * frictionY;
+        } else {
+            this.dy += gravity;
+        }
+
+        if (this.x - this.radius <= 0) {
+            this.dx = -this.dx;
+        }
+        if (this.x + this.radius + this.dx > canvas.width) {
+            this.dx = -this.dx;
+        }
+
+        this.dx = this.dx * 0.999;
+
+        this.y += this.dy;
+        this.x += this.dx;
 
         this.draw();
     }
 }
 
 let circles = []
-for (let i = 0; i < 15; i++) {
-    const radius = 100;
-    const x = Math.random() * (canvas.width - radius * 2) + radius;
-    const y = Math.random() * (canvas.height - radius * 2) + radius;;
-    const dx = (Math.random() -  0.5) * 10;
+for (let i = 0; i < 1000; i++) {
+    const radius = 50;
+    const x = Math.random() * (canvas.width - radius * 2 - 300) + radius + 100;
+    const y = Math.random() * (canvas.height - radius * 2) + radius;
+    const dx = (Math.random() -  0.5) * 40;
     const dy = (Math.random() -  0.5) * 10;
 
     circles.push(new Circle(x, y, dx, dy, radius));
@@ -49,6 +69,7 @@ function animate() {
 
     for (let i = 0; i < circles.length; i++) {
         circles[i].update();
+        console.log(`${i}: ${Math.round(circles[i].dx)} ${Math.round(circles[i].dy)}`);
     }
 
     
